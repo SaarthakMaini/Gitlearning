74bec4d434020975c8ca83dc6c05d8d039710d8d
Saarthak <saarthakmaini@gmail.com>
2022-01-21 11:14:39 +0530

    clearing repository status

---
+++ src/com/saarthak/Main.java
@@ -0,0 +1,14 @@
+package com.saarthak;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        Scanner
+        System.out.println();
+        // write your code here
+
+    }
+}
